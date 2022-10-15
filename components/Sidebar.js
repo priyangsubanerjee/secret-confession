8f58d6a9c4fbe07de2dc427d12821dca27770075
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-10-15 19:25:17 +0530

    inst

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 function Sidebar({ open, close }) {
   const shareLink = async () => {
@@ -16,62 +17,75 @@ function Sidebar({ open, close }) {
   };
 
   return (
-    <>
-      {open && (
-        <div className="fixed inset-0 bg-black/50 flex z-10">
-          <div className="h-full w-[70%] bg-white">
-            <div className="px-5 py-4 flex items-center justify-end">
-              <button onClick={() => close()}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="w-5 h-5"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <ul className="text-xs text-gray-500 py-4 space-y-3 border-b">
-              <li className="py-1 px-4 flex space-x-2">
-                <span>Terms & Conditions</span>
-              </li>
-              <li className="py-1 px-4 flex space-x-2">
-                <span>Privacy policy</span>
-              </li>
-              <li className="py-1 px-4 flex space-x-2">
-                <span>Contact us</span>
-              </li>
-              <li className="py-1 px-4 flex space-x-2">
-                <Link href="https://www.linkedin.com/in/priyangsu-banerjee/">
-                  <span>Developers profile</span>
-                </Link>
-              </li>
+    <div className="fixed">
+      <Fade when={open}>
+        {open && <div className="bg-black/50 flex z-10 fixed inset-0 "></div>}
+      </Fade>
 
-              <li className="py-1 px-4 flex space-x-2">
-                <button
-                  onClick={() => shareLink()}
-                  className="w-full text-sm p-2 bg-teal-500 active:bg-teal-600 text-white rounded font-medium"
-                >
-                  Share this app
+      <Fade when={open} left>
+        {open && (
+          <div className="fixed z-20 inset-0 flex">
+            <div className="h-full w-[70%] bg-white">
+              <div className="px-5 py-4 flex items-center justify-end">
+                <button onClick={() => close()}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="w-5 h-5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
                 </button>
-              </li>
-            </ul>
-            <ul className="text-xs text-gray-500 py-4 space-y-3">
-              <li className="py-1 px-4 flex space-x-2 opacity-50">
-                <span>Download instagram story cover.</span>
-              </li>
-            </ul>
+              </div>
+              <ul className="text-xs text-gray-500 py-4 space-y-3 border-b">
+                <li className="py-1 px-4 flex space-x-2">
+                  <span>Terms & Conditions</span>
+                </li>
+                <li className="py-1 px-4 flex space-x-2">
+                  <span>Privacy policy</span>
+                </li>
+                <li className="py-1 px-4 flex space-x-2">
+                  <span>Contact us</span>
+                </li>
+                <li className="py-1 px-4 flex space-x-2">
+                  <Link href="https://www.linkedin.com/in/priyangsu-banerjee/">
+                    <span>Developers</span>
+                  </Link>
+                </li>
+
+                <li className="py-1 px-4 flex space-x-2">
+                  <button
+                    onClick={() => shareLink()}
+                    className="w-full text-sm p-2 bg-teal-500 active:bg-teal-600 text-white rounded font-medium"
+                  >
+                    Share this app
+                  </button>
+                </li>
+              </ul>
+              <ul className="text-xs text-gray-500 py-4 space-y-3">
+                <li className="py-1 px-4 flex space-x-2 opacity-50">
+                  <a
+                    href="/instagramstory.png"
+                    download={true}
+                    rel="noopener noreferrer"
+                    name="Instagram story cover"
+                  >
+                    <span>Download instagram story cover.</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      )}
-    </>
+        )}
+      </Fade>
+    </div>
   );
 }
 
