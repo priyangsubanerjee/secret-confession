a43a7f3a5f9ee75436621416946ed81764bb4672
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-10-17 16:42:44 +0530

    portfolio

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,9 @@ function Footer() {
     <div className="flex flex-col lg:flex-row lg:items-center space-y-3 lg:space-y-0 lg:space-x-20 items-center justify-center mt-10 mb-10">
       <div className="text-sm text-zinc-700">
         made with <span className="text-rose-500">&hearts;</span> by{" "}
-        <span className="text-teal-600 font-medium">Priyangsu Banerjee</span>
+        <span className="text-teal-600 font-medium">
+          <a href="https://priyangsu.vercel.app">Priyangsu Banerjee</a>
+        </span>
       </div>
       <div className="text-xs text-zinc-500">
         All right reserved @ Virtual Base Corp.
